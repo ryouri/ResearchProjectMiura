144f3033e6565646cf1c4873653ca6af24a4c8bc
misawa <misawa@133.10.236.199>
2015-02-06 12:32:41 +0900

    描画ウィンドウサイズを固定することに変更

--- src/researchproject/drawmodule/DrawManager.java
+++ src/researchproject/drawmodule/DrawManager.java
@@ -31,20 +31,20 @@ public class DrawManager {
 		}
 		//4変数
 		//カルノー図描画クラスを生成し，Mass管理クラスを渡す
-//		KarnaughMapVariable4Drawer karnaughMapDrawer = new KarnaughMapVariable4Drawer();
-//		karnaughMapDrawer.setMassManager(massManager);
-//		karnaughMapDrawer.setCalcurateManager(calcurateManager);
-//		karnaughMapDrawer.init();
-
-		//矢部さんの5変数プログラムを実行
-		KarnaughMapVariable5Drawer karnaughMapDrawer = new KarnaughMapVariable5Drawer();
+		KarnaughMapVariable4Drawer karnaughMapDrawer = new KarnaughMapVariable4Drawer();
 		karnaughMapDrawer.setMassManager(massManager);
 		karnaughMapDrawer.setCalcurateManager(calcurateManager);
 		karnaughMapDrawer.init();
 
+		//矢部さんの5変数プログラムを実行
+//		KarnaughMapVariable5Drawer karnaughMapDrawer = new KarnaughMapVariable5Drawer();
+//		karnaughMapDrawer.setMassManager(massManager);
+//		karnaughMapDrawer.setCalcurateManager(calcurateManager);
+//		karnaughMapDrawer.init();
+
 		//JFrameに貼り付けることで，JAppletを表示する
 		JFrame frame = new JFrame("Test");
-		frame.setSize(karnaughMapDrawer.getWidth(), karnaughMapDrawer.getHeight()); // サイズは適当。上で作ったインスタンスからゲットするのもいいと思います。
+		frame.setSize(500, 500); // サイズは適当。上で作ったインスタンスからゲットするのもいいと思います。
 		frame.add(karnaughMapDrawer);
 		frame.setVisible(true);
 	}
